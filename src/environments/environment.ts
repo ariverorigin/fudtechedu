67862aed94c58b038b0898d28d84f03e687271c6
ariver <genesisrivera.work@outlook.ph>
2021-07-06 17:28:25 +0800

    Change api keys and secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   api: 'https://foodchem-edu.com/',
-  api_key: 'ck_d777339a7cf7f3f6caeaa04ff7fdbe2aec6f72bd',
-  api_secret: 'cs_70d330d82f0d4d50e23c8eced96a1e4c4e89d9b9',
+  api_key: 'ck_3e2d0244049262363d18338d0dceae060aab2732',
+  api_secret: 'cs_2151aabdb9c284ed584cc2268bd5d060addf8559',
   cache_folder: 'CACHED-IMG',
   heroku: 'https://tim-canoe-44427.herokuapp.com/',
 };
